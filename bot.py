ac80b6d54e6f821d0699e910c9672a6a8c45cb1f
Alex Kott <alexey.kott@gmail.com>
2019-04-30 01:13:45 +0300

    Complete

--- bot.py
+++ bot.py
@@ -1,24 +1,34 @@
+import json
 import logging
+from asyncio import sleep
+from datetime import datetime, timedelta
+
 from random import randint
 
 from aiogram import Bot, Dispatcher
-from aiogram.types import Message, ContentType
+from aiogram.types import Message, ContentType, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery, \
+    InputMediaAnimation, InputMediaPhoto
 from aiogram.utils import executor
+from aiogram.utils.exceptions import MessageToDeleteNotFound, MessageCantBeDeleted
 from aiohttp import BasicAuth
 import requests
+from requests.exceptions import ConnectionError
 
 from config import BOT_TOKEN, PROXY_HOST, PROXY_PASS, PROXY_PORT, PROXY_USERNAME
-
-# Configure logging
 from models import Chat
 
 logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger()
+
+file_handler = logging.FileHandler('info.log')
+file_handler.setLevel(logging.INFO)
+logger.addHandler(file_handler)
 
 try:
     PROXY_AUTH = None
     PROXY_URL = None
     response = requests.get('https://api.telegram.org')
-except Exception:
+except ConnectionError as e:
     PROXY_URL = f"socks5://{PROXY_HOST}:{PROXY_PORT}"
     PROXY_AUTH = BasicAuth(login=PROXY_USERNAME, password=PROXY_PASS)
 bot = Bot(token=BOT_TOKEN, proxy=PROXY_URL, proxy_auth=PROXY_AUTH)
@@ -34,28 +44,62 @@ async def ping_handler(message: Message):
     await message.reply("I'm alive")
 
 
-@dp.message_handler(commands=['crack'])
-async def crack_handler(message: Message):
-    print(message)
+@dp.callback_query_handler()
+async def callback_handler(callback: CallbackQuery):
+    chat = Chat.get_by_callback(callback)
+    chat.last_crack = datetime.now()
+    chat.save()
+    logger.info(callback)
 
-    chat = Chat.get(chat_id=message.chat.id)
-    for i in range(1, chat.last_message_id):
+    with open('./assets/glove/snap.gif', 'rb') as file:
+        await bot.edit_message_media(chat_id=chat.id,
+                                     message_id=callback.message.message_id,
+                                     media=InputMediaAnimation(file))
+
+    await sleep(2.9)
+    with open('./assets/glove/glove180.png', 'rb') as file:
+        await bot.edit_message_media(chat_id=chat.id,
+                                     message_id=callback.message.message_id,
+                                     media=InputMediaPhoto(file))
+
+    await sleep(3)
+
+    for i in range(chat.last_message_id, chat.last_message_id-100, -1):
         if randint(1, 2) == 2:
-            print(i)
-    # await bot.delete_message()
+            await sleep(1)
+            try:
+                await bot.delete_message(chat_id=chat.id, message_id=i)
+            except MessageToDeleteNotFound:
+                pass
+            except MessageCantBeDeleted:
+                pass
+
+
+@dp.message_handler(commands=['crack', 'snap'])
+async def crack_handler(message: Message):
+    chat = Chat.get_by_message(message)
+
+    if chat.last_crack is None:
+        pass
+    elif chat.last_crack > (datetime.now() - timedelta(hours=8)):
+        await message.reply("The time is not come yet")
+        return
+
+    keyboard = InlineKeyboardMarkup()
+
+    callback_data = json.dumps({
+        'data': 'tony stark will die'
+    })
+    button = InlineKeyboardButton('Snap', callback_data=callback_data)
+    keyboard.row(button)
+    with open('./assets/glove/glove180.png', 'rb') as file:
+        await bot.send_photo(chat.id, file, reply_markup=keyboard)
 
 
 @dp.message_handler(content_types=[ContentType.TEXT])
 async def text_handler(message: Message):
-
-    print(message)
-
-    chat = Chat.get_by_message(message)
-
-    print(chat)
-    # chat.last_message_id = message.message_id
-    # chat.save()
-
+    Chat.get_by_message(message)
+    logger.info(message)
 
 
 if __name__ == "__main__":
--- models.py
+++ models.py
@@ -1,5 +1,5 @@
-from aiogram.types import Message
-from peewee import SqliteDatabase, Model, IntegerField, TextField, DateTimeField
+from aiogram.types import Message, CallbackQuery
+from peewee import SqliteDatabase, Model, IntegerField, TextField, DateTimeField, IntegrityError
 
 
 class BaseClass(Model):
@@ -8,14 +8,25 @@ class BaseClass(Model):
 
 
 class Chat(BaseClass):
-    chat_id = IntegerField(primary_key=True)
     name = TextField()
     last_message_id = IntegerField()
     last_crack = DateTimeField(null=True)
 
     @classmethod
     def get_by_message(cls, message: Message):
-        chat = Chat.get_or_create(chat_id=message.chat.id,
-                                  name=message.chat.full_name,
-                                  last_message_id=message.message_id)
+        try:
+            chat, created = Chat.get_or_create(id=message.chat.id,
+                                               name=message.chat.full_name,
+                                               last_message_id=message.message_id)
+        except IntegrityError:
+            chat = Chat.get(id=message.chat.id)
 
+        chat.last_message_id = message.message_id
+        chat.save()
+
+        return chat
+
+    @classmethod
+    def get_by_callback(cls, callback: CallbackQuery):
+
+        return Chat.get(id=callback.message.chat.id)
